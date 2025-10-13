c7a2ee5fee9cbebc4f85fbca96b522e58682da31
Mochammad Farros Fatchur Roji <farros.alfatih@gmail.com>
2025-10-13 14:58:01 +0700

    up: search box

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,17 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import pagefind from 'astro-pagefind';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://farrosfr.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx(), sitemap(), pagefind()],
+    vite: {
+        build: {
+            rollupOptions: {
+                external: ['/pagefind/pagefind.js']
+            }
+        }
+    }
 });
