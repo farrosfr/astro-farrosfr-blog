3e80dc65c2f10eca986b81ed85d004bb007a78c0
Mochammad Farros Fatchur Roji <farros.alfatih@gmail.com>
2025-11-04 12:43:54 +0700

    feat: tailwing 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,18 +5,23 @@ import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 import pagefind from 'astro-pagefind';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://farrosfr.com',
-	integrations: [mdx(), sitemap(), pagefind()],
+    site: 'https://farrosfr.com',
+    integrations: [mdx(), sitemap(), pagefind()],
     vite: {
-        build: {
-            rollupOptions: {
-                external: ['/pagefind/pagefind.js']
-            }
-        },
-        ssr: {
-            external: ['/pagefind/pagefind.js']
-        }
+      build: {
+          rollupOptions: {
+              external: ['/pagefind/pagefind.js']
+          }
+      },
+
+      ssr: {
+          external: ['/pagefind/pagefind.js']
+      },
+
+      plugins: [tailwindcss()]
     }
 });
